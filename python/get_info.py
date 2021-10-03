901cdce3a7d01a6cc20a453f712d6ce4379ed1cb
Justine Picar <ohhmannjustine@gmail.com>
2021-10-03 16:02:11 -0700

    updates to files

--- python/get_info.py
+++ python/get_info.py
@@ -1,24 +1,56 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def plot_hists(data, title):
-    '''
+    """
     Plots histogram of the data inputted
     :param data: dataset
     :param title: title of the collection of histograms
     :return: None
+    """
+
+    edit = data.iloc[:, 1:-1]
+
+    sns.set()
+    plt.figure(figsize=(20, 20))
+
+    for i in range(len(edit.columns)):
+        ax = plt.subplot(int(len(edit.columns) / 6), 6, i + 1)
+        _ = sns.histplot(data=data, x=edit.columns[i], ax=ax, hue='label')
+        _ = plt.title(edit.columns[i])
+        _ = plt.xticks(rotation=90)
+        _ = plt.ylabel('Count')
+    _ = plt.tight_layout()
+    _ = plt.suptitle(f'{title}', y=1.02, size=24)
+    _ = plt.show()
+
+    return None
+
+
+def plot_violin(data, title):
+    '''
+    Plots violin plot of the data inputted
+    :param data: dataset
+    :param title: title of the collection of violin plots
+    :return: None
     '''
     plt.figure(figsize=(20, 20))
-    
-    for i in range(len(data.columns)):
-        ax = plt.subplot(int(len(data.columns)/6), 6, i + 1)
-        plt.hist(data.iloc[:, i].dropna())
-        plt.title(data.columns[i])
+
+    edit = data.iloc[:, 1:-2]
+
+    for i in range(len(edit.columns)):
+        ax = plt.subplot(int(len(edit.columns) / 6), 6, i + 1)
+        sns.violinplot(x='all', y=edit.columns[i], data=data, ax=ax, hue='label', inner='quartile', palette='muted',
+                       split=True)
+        plt.title(edit.columns[i])
         plt.xticks(rotation=90)
         plt.ylabel('Count')
     plt.tight_layout()
     plt.suptitle(f'{title}', y=1.02, size=24)
     plt.show()
     return None
+
 
 def get_missing_data(data):
     '''
@@ -29,6 +61,7 @@ def get_missing_data(data):
     missing = round((data.isnull().sum() / len(data) * 100), 2)
     return print(f'% of Missing Data:\n{missing}\n')
 
+
 def get_type(type):
     '''
     Determines whether dataset is a train or validation set and checks for errors in user input
@@ -36,7 +69,7 @@ def get_type(type):
     :return: string of type or error
     '''
     if type == 'test' or type == 'validation':
-        return'val'
+        return 'val'
     elif type == 'train':
         return 'train'
     else:
